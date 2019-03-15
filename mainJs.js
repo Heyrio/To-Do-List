bd0fa7a3e6f7e8d9d6c9b31e4abe55c5df2995fa
Hamza Amer <hamer1563@gmail.com>
2019-03-15 13:41:52 -0400

    Create mainJs.js

---
+++ mainJs.js
@@ -0,0 +1,33 @@
+// check off to do by clicking
+
+$("ul").on("click", "li", function() {
+
+	$(this).toggleClass("completed");
+
+});
+
+// click on x to delete to do
+
+$("ul").on("click", "span", function(e) {
+
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	 e.stopPropagation();
+});
+
+
+$("input[type='text']").keypress(function(e){
+
+	if(e.which === 13){
+
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span> X </span>" + todoText + "</li>");
+		
+
+
+	}
+
+
+});
